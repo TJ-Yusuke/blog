60c455fcf7a11346d5017cf0a60b8910912f35fe
TJ-Yusuke <jeeproud@gmail.com>
2020-08-25 23:00:41 +0900

    add: hirizontalCard追加

---
+++ components/molecules/horizontalCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Url } from '../../logic/domain/entity/types/url';
+
+type Props = {
+  thumbnail: Url;
+  category: string;
+  title: string;
+  outline: string;
+  date: string;
+};
+export const HorizontalCard: React.FC<Props> = ({
+  thumbnail,
+  category,
+  title,
+  outline,
+  date,
+}: Props) => {
+  return (
+    <div className="max-w-md w-full lg:flex transform transition-all duration-300 scale-100 hover:scale-95">
+      <div
+        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+        style={{ backgroundImage: `url(${thumbnail}` }}
+        title={title}
+      />
+      <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          <p className="text-sm text-grey-dark flex items-center">
+            #{category}
+          </p>
+          <div className="text-black font-bold text-xl mb-2">{title}</div>
+          <p className="text-grey-darker text-base">{outline}</p>
+        </div>
+        <div className="flex items-center">
+          <div className="text-sm">
+            {/*<p className="text-black leading-none">Jonathan Reinink</p>*/}
+            <p className="text-grey-dark">{date}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/whiteboard.tsx
+++ pages/whiteboard.tsx
@@ -3,10 +3,22 @@ import React from 'react';
 import { StickyFooter } from '../components/organisms/footer';
 import { ArticlesList } from '../components/molecules/articlesList';
 import { ArticlesCard } from '../components/molecules/articlesCard';
+import { HorizontalCard } from '../components/molecules/horizontalCard';
 
 const Whiteboard: React.FC<any> = () => {
   return (
     <>
+      <HorizontalCard
+        thumbnail={
+          'https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        }
+        category={'エンジニアリング'}
+        title={'今日の積み重ね'}
+        outline={
+          '今日というひはあなたは何をした？私は何をしたのか？日々吸収して成長しなければいけない。それが将来なんの形になるかはわからないが。'
+        }
+        date={'2020/08/25'}
+      />
       <ArticlesCard
         thumbnail={
           'https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
