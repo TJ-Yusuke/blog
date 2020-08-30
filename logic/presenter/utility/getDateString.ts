fe65ab695bc096621a33729f005d7f7f6ffb0c4d
TJ-Yusuke <jeeproud@gmail.com>
2020-08-30 10:55:07 +0900

    add: 日付文字列作成、プレーンテキスト作成、

---
+++ logic/presenter/utility/getDateString.ts
@@ -0,0 +1,8 @@
+/**
+ * 日付文字列作成
+ * @param date: Date型日付
+ * @string: 日付文字列
+ */
+export const getDateString = (date: Date): string => {
+  return `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`;
+};
---
+++ logic/presenter/utility/getPlainText.ts
@@ -0,0 +1,17 @@
+/**
+ * プレーンテキスト作成
+ * @param text: html文字列
+ * @return text: プレーンテキスト
+ */
+export const getPlainText = (text: string): string => {
+  text = text.replace(/<style([\s\S]*?)<\/style>/gi, '');
+  text = text.replace(/<script([\s\S]*?)<\/script>/gi, '');
+  text = text.replace(/<\/div>/gi, '\n');
+  text = text.replace(/<\/li>/gi, '\n');
+  text = text.replace(/<li>/gi, '  *  ');
+  text = text.replace(/<\/ul>/gi, '\n');
+  text = text.replace(/<\/p>/gi, '\n');
+  text = text.replace(/<br\s*[\/]?>/gi, '\n');
+  text = text.replace(/<[^>]+>/gi, '');
+  return text;
+};
