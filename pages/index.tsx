0d00693184dcd6b262e91875413ba7cd67f1f935
TJ-Yusuke <jeeproud@gmail.com>
2020-08-30 12:17:07 +0900

    refs: Link範囲拡大

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,39 +25,41 @@ const Index = ({ articlesJson }) => {
               : 'md:w-1/3'
           } p-6 flex flex-col flex-grow flex-shrink`}
         >
-          <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-            <Link href="posts/[id]" as={`posts/${article.id}`}>
-              <a className="flex flex-wrap no-underline hover:no-underline">
-                <img
-                  src={
-                    article.thumbnail
-                      ? article.thumbnail.url
-                      : 'https://source.unsplash.com/collection/225/800x600'
-                  }
-                  className={`${
-                    i == 0 || i == 1 || i == 2 ? 'h-64' : 'h-full'
-                  } w-full object-cover rounded-t pb-6`}
-                  alt={article.title}
-                />
-                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
-                  {article.category.name}
-                </p>
-                <div className="w-full font-bold text-xl text-gray-900 px-6">
-                  {article.title}
+          <Link href="posts/[id]" as={`posts/${article.id}`}>
+            <a>
+              <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                <div className="flex flex-wrap no-underline hover:no-underline">
+                  <img
+                    src={
+                      article.thumbnail
+                        ? article.thumbnail.url
+                        : 'https://source.unsplash.com/collection/225/800x600'
+                    }
+                    className={`${
+                      i == 0 || i == 1 || i == 2 ? 'h-64' : 'h-full'
+                    } w-full object-cover rounded-t pb-6`}
+                    alt={article.title}
+                  />
+                  <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                    {article.category.name}
+                  </p>
+                  <div className="w-full font-bold text-xl text-gray-900 px-6">
+                    {article.title}
+                  </div>
+                  <p className="text-gray-800 font-serif text-base px-6 mb-5">
+                    {article.body}
+                  </p>
                 </div>
-                <p className="text-gray-800 font-serif text-base px-6 mb-5">
-                  {article.body}
-                </p>
-              </a>
-            </Link>
-          </div>
-          <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-            <div className="flex items-center justify-between">
-              <p className="text-gray-600 text-xs md:text-sm">
-                {article.updatedAt}
-              </p>
-            </div>
-          </div>
+              </div>
+              <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                <div className="flex items-center justify-between">
+                  <p className="text-gray-600 text-xs md:text-sm">
+                    {article.updatedAt}
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Link>
         </div>
       );
     });
