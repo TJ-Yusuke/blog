f972455589f725ce69d4ccc0b6b9a7f64c52e129
TJ-Yusuke <jeeproud@gmail.com>
2020-12-05 23:49:58 +0900

    add: 記事一覧ページ追加

---
+++ pages/archive/index.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next';
+import { ArticlePresenterInstance } from 'components/utility/instance/logic';
+import { ArticleRequestType } from 'logic/interface/presenter/articlePresenterInterface';
+import React from 'react';
+import { Header } from 'components/organisms/header';
+import { Footer } from 'components/organisms/footer';
+import { ArchiveTable } from 'components/organisms/archiveTable';
+
+const Index = ({ articlesJson }) => {
+  const articles = JSON.parse(articlesJson);
+  return (
+    <body className="bg-gray-100 font-gothic leading-normal tracking-normal">
+      <Header />
+      <div className="mt-16 container mx-auto md:px-32">
+        <ArchiveTable articles={articles} />
+      </div>
+      <Footer />
+    </body>
+  );
+};
+export default Index;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const articles = await ArticlePresenterInstance.fetchArticles(
+    ArticleRequestType.ARCHIVE
+  ).catch((error) => {
+    throw error;
+  });
+  const articlesJson = JSON.stringify(articles.body.data);
+  return {
+    props: {
+      articlesJson,
+    },
+    revalidate: 1,
+  };
+};
